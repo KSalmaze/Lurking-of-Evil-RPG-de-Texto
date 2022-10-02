5899bd4faf9f4ffa79a70b149e9d114cf67c3c31
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2022-10-02 00:25:48 -0300

    fé

---
+++ New/TLOE/Main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int character_creation ();
+void basic ();
+
+typedef struct Satributes {
+int teste;
+int life;
+int strengh;
+int intelligence;
+int sanity;
+int charisma;
+int resistance;
+int luck;
+};
+
+typedef struct Splayer {
+struct Satributes atributes;
+char arma[20];
+
+};
+
+int main () {
+
+return 0;
+}
+
+void basic (){}
+
+int character_creation (){
+}
