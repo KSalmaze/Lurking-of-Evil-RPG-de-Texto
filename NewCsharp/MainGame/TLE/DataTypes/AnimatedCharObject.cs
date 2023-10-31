e7d3f9f65852cc89bde316b17aff6dcb0c28c44d
KSalmaze <pedrohsalmaze@gmail.com>
2023-10-31 18:43:46 -0300

    V 0.1.4 Animation Test

--- NewCsharp/MainGame/TLE/DataTypes/AnimatedCharObject.cs
+++ NewCsharp/MainGame/TLE/DataTypes/AnimatedCharObject.cs
@@ -3,15 +3,30 @@ namespace TLE.DataTypes;
 public class AnimatedCharObject : CharObject
 {
     private List<Sprite> _frames;
-    private int _currentFrame = 0;
+    private int _currentFrame;
 
-    public AnimatedCharObject(string[][] frames) : base(new[] { "WWW", "WWW", "WWW" })
+    public AnimatedCharObject(Sprite[] frames) : base(new[] { "WWW", "WWW", "WWW" })
     {
+        _frames = new List<Sprite>();
         
+        for (int i = 0; i < frames.Length; i++)
+        {
+            _frames.Add(frames[i]);
+        }
+
+        sprite = _frames[_currentFrame].sprite;
     }
 
     public void NextFrame()
     {
-        
+        if (_currentFrame < _frames.Count)
+        {
+            _currentFrame++;
+            sprite = _frames[_currentFrame].sprite;
+            return;
+        }
+
+        _currentFrame = 0;
+        sprite = _frames[_currentFrame].sprite;
     }
 }
